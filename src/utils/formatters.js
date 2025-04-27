6fdf358f14902e84cd20299c00695dc7f14c2475
Mario <team.miningzone@gmail.com>
2025-04-28 00:48:56 +0200

    chore(utils): añadir helper formatPrice

---
+++ src/utils/formatters.js
@@ -0,0 +1,7 @@
+export function formatPrice(amount) {
+    return new Intl.NumberFormat('es-ES', {
+      style: 'currency',
+      currency: 'EUR',
+      minimumFractionDigits: 2
+    }).format(amount);
+  }
